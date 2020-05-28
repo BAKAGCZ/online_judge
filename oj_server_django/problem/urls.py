67f21e59d4a406270424bf632e029ac3cc179abb
BAKAGCZ <625937451@qq.com>
2020-05-29 04:46:36 +0800

    update 0.41

--- oj_server_django/problem/urls.py
+++ oj_server_django/problem/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('list/', views.problemlist, name='problemlist'),
+    path('list/<int:pindex>/', views.problemlist, name='problemlist'),
     path('<int:problem_id>/', views.problem, name='problem'),
-    path('search/', views.search, name='search'),
+    path('search/<int:pindex>/', views.search, name='search'),
     path('submit/<int:problem_id>/', views.submit, name='submit'),
 ]
--- oj_server_django/problem/views.py
+++ oj_server_django/problem/views.py
@@ -1,43 +1,46 @@
 from django.shortcuts import render
 from .models import Problem
-# from django.core.paginator import Paginator
-
-# def problemlist(request):
-#     problem_list = Problem.objects.all()
-#     paginator = Paginator(problem_list, 5)
-#     if pindex == "":  # django默认返回空值，设置默认值1
-#         pindex = 1
-#     else:  # 如果有返回值，把返回值转为整数型
-#         int(pindex)
-#     page = paginator.page(pindex)  # 传递当前页的实例对象到前端
-#     context = {'page': page}
-#     return render(request, 'problem/list.html', context=context)
+from django.core.paginator import Paginator
 
 
-def problemlist(request):
-    context = {'problem_list': Problem.objects.all()}
+def problemlist(request, pindex):
+    problem_list = Problem.objects.all()
+    paginator = Paginator(problem_list, 1)
+    if pindex == "":  # django默认返回空值，设置默认值1
+        pindex = 1
+    else:  # 如果有返回值，把返回值转为整数型
+        int(pindex)
+    page = paginator.page(pindex)  # 传递当前页的实例对象到前端
+    context = {'page': page, 'type': 'list'}
     return render(request, 'problem/list.html', context=context)
 
 
-def search(request):
+def search(request, pindex):
     if request.method == 'POST':  # 0 "" All All
         search_range = request.POST['search_range']
         search_string = request.POST['search_string']
+    elif request.method == 'GET':
+        search_range = request.GET['search_range']
+        search_string = request.GET['search_string']
 
-    problemResObj = Problem.objects
+    problem_res_obj = Problem.objects
     if search_string != None and search_string != "":
         if search_range == "problem_id":
-            problemResObj = problemResObj.filter(id=search_string)
+            problem_res_obj = problem_res_obj.filter(id=search_string)
         if search_range == "title":
-            problemResObj = problemResObj.filter(
+            problem_res_obj = problem_res_obj.filter(
                 title__icontains=search_string)
         if search_range == "point":
-            problemResObj = problemResObj.filter(point=search_string)
-        context = {'problem_list': problemResObj,
-                   'search_range': search_range, 'search_string': search_string}
-    else:
-        context = {'problem_list': problemResObj.all(),
-                   'search_range': search_range, 'search_string': search_string}
+            problem_res_obj = problem_res_obj.filter(point=search_string)
+
+    paginator = Paginator(problem_res_obj, 1)
+    if pindex == "":  # django默认返回空值，设置默认值1
+        pindex = 1
+    else:  # 如果有返回值，把返回值转为整数型
+        int(pindex)
+    page = paginator.page(pindex)  # 传递当前页的实例对象到前端
+    context = {'page': page, 'type': 'search',
+               'search_range': search_range, 'search_string': search_string}
     return render(request, 'problem/list.html', context=context)
 
 
